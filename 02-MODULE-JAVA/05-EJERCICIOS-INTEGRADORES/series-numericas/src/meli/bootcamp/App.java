27d06efefc10ed6351ca751329b8afa5e16b429d
faacosta_meli <facundo.acosta@mercadolibre.com>
2025-03-04 19:54:03 -0300

    Optimizo App de Series Numéricas

--- 02-MODULE-JAVA/05-EJERCICIOS-INTEGRADORES/series-numericas/src/meli/bootcamp/App.java
+++ 02-MODULE-JAVA/05-EJERCICIOS-INTEGRADORES/series-numericas/src/meli/bootcamp/App.java
@@ -4,47 +4,42 @@ import meli.bootcamp.models.series.IntegerSerieProgresiva;
 
 public class App {
 
+    private static final String RESET = "\u001B[0m";
+
+    private static final String BLUE = "\u001B[34m";
+    
+    private static final String MAGENTA = "\u001B[35m";
+    
+    private static final String CYAN = "\u001B[36m";
+
+    private static final String SEPARADOR = "=".repeat(60);
+
+    private static void imprimirSerie(String titulo, String color, IntegerSerieProgresiva serie) {
+        System.out.println(color + titulo);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(String.format("  - %s", serie.valorSiguiente()));
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("=".repeat(60));
-        System.out.println("SERIES PROGRESIVAS");
-        System.out.println("=".repeat(60));
+        System.out.println(String.format("%s%n%s%n%s", SEPARADOR, "SERIES PROGRESIVAS", SEPARADOR));
 
-        System.out.print("\u001B[34m");
-        System.out.println("SERIE PROGRESIVA INTEGER (VALOR INICIAL 2, PASO 2)");
+        IntegerSerieProgresiva integerSerieProgresiva = new IntegerSerieProgresiva(2, 2);
+        imprimirSerie("SERIE PROGRESIVA INTEGER (VALOR INICIAL 2, PASO 2)", BLUE, integerSerieProgresiva);
 
-        IntegerSerieProgresiva integer = new IntegerSerieProgresiva(2, 2);
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 2
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 4
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 6
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 8
+        System.out.println(RESET + "-".repeat(60));
 
-        System.out.print("\u001B[0m");
-        System.out.println("-".repeat(60));
-        
-        System.out.print("\u001B[35m");
-        System.out.println("SERIE PROGRESIVA INTEGER (VALOR INICIAL 1, PASO 2)");
+        integerSerieProgresiva.setValorInicial(1);
+        imprimirSerie("SERIE PROGRESIVA INTEGER (VALOR INICIAL 1, PASO 2)", MAGENTA, integerSerieProgresiva);
 
-        integer.setValorInicial(1);
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 3
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 5
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 7
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 9
+        System.out.println(RESET + "-".repeat(60));
 
-        System.out.print("\u001B[0m");
-        System.out.println("-".repeat(60));
+        integerSerieProgresiva = new IntegerSerieProgresiva(3, 3);
+        imprimirSerie("SERIE PROGRESIVA INTEGER (VALOR INICIAL 3, PASO 3)", CYAN, integerSerieProgresiva);
 
-        System.out.print("\u001B[36m");
-        System.out.println("SERIE PROGRESIVA INTEGER (VALOR INICIAL 3, PASO 3)");
-
-        integer = new IntegerSerieProgresiva(3, 3);
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 3
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 6
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 9
-        System.out.println(String.format("  - %s", integer.valorSiguiente())); // - 12
-
-        System.out.print("\u001B[0m");
-        System.out.println("=".repeat(60));
+        System.out.println(RESET + "=".repeat(60));
 
     }
 
